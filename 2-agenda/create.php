bc1a988022f52e8c7bd215901d64cf6c27576c29
Larissa Narciso <97248900+larisnarciso@users.noreply.github.com>
2023-05-06 17:07:21 -0300

    Delete 2-agenda directory

--- 2-agenda/create.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  include_once ("templates/header.php");
-?>
-
-  <h1>Criando Contato</h1>
-
-<?php 
-  include_once ("templates/footer.php");
-?>
--- 2-agenda/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  include_once ("templates/header.php");
-?>
-
-  <h1>Agenda</h1>
-
-<?php 
-  include_once ("templates/footer.php");
-?>
