2bf92a85030590c6e90587161c71419aba4b6757
Larissa Narciso <larisnarciso@gmail.com>
2023-05-06 21:36:40 -0300

    Funcionalidade de inserir contados na agenda realizada

--- 2-agenda/config/process.php
+++ 2-agenda/config/process.php
@@ -5,33 +5,72 @@
   include_once("connection.php");
   include_once("url.php");
 
-  $id;
-  
-  if(!empty($_GET)){
-    $id = $_GET["id"];
-  }
-  // Retorna dados de um contato
-  if(!empty($id)){
+  $data = $_POST;
 
-    $query = "SELECT * FROM contacts WHERE id = :id";
+  // MODIFICAÇÕES NO BANCO
+  if(!empty($data)){
 
-    $stmt = $conn->prepare($query);
+    // CRIAR CONTATOS
+    if($data["type"] === "create" ){
+      $name = $data["name"];
+      $phone = $data["phone"];
+      $observations = $data["observations"];
 
-    $stmt->bindParam(":id", $id);
+      $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
 
-    $stmt->execute();
-    $contact = $stmt->fetch();
+      $stmt = $conn->prepare($query);
 
+      $stmt->bindParam(":name", $name);
+      $stmt->bindParam(":phone", $phone);
+      $stmt->bindParam(":observations", $observations);
+
+      try{
+       
+        $stmt->execute();
+        $_SESSION["msg"] = "Contato criado com sucesso!";
+    
+      } catch (PDOException $e){
+        // Erro na conexão
+        $error = $e->getMessage();
+        echo "Erro: $error";
+      }
+
+    }
+
+    // REDIRECT HOME
+    header("Location:" . $BASE_URL . "../index.php");
+
+  // SELEÇÃO DE DADOS
   }else{
-    // Retorna todos os contatos
-    $contacts = [];
+    $id;
+    
+    if(!empty($_GET)){
+      $id = $_GET["id"];
+    }
+    // RETORNA DADOS DE UM CONTATO
+    if(!empty($id)){
   
-    $query = "SELECT * FROM contacts";
+      $query = "SELECT * FROM contacts WHERE id = :id";
   
-    $stmt = $conn->prepare($query);
+      $stmt = $conn->prepare($query);
   
-    $stmt->execute();
-    $contacts = $stmt->fetchAll();
+      $stmt->bindParam(":id", $id);
+  
+      $stmt->execute();
+      $contact = $stmt->fetch();
+  
+    }else{
+      // RETORNA TODOS OS CONTATOS
+      $contacts = [];
+    
+      $query = "SELECT * FROM contacts";
+    
+      $stmt = $conn->prepare($query);
+    
+      $stmt->execute();
+      $contacts = $stmt->fetchAll();
+    }
   }
 
-?>
+  // FECHAR CONEXÃO
+  $conn = null;
--- 2-agenda/templates/header.php
+++ 2-agenda/templates/header.php
@@ -18,7 +18,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Agenda de Contatos</title>
   <!-- BOOTSTRAP -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
   <!-- FONT-AWESOME -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- CSS -->
