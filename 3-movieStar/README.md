28449b23c58a8de7d96fa5c17b70e9ff46ffbefe
Larissa Narciso <larisnarciso@gmail.com>
2023-05-07 03:48:14 -0300

    Primeiro commit: iniciando projeto

---
+++ 3-movieStar/README.md
@@ -0,0 +1,21 @@
+# Movie Star
+
+![Movie Star](moviestar.jpg)
+
+Projeto Movie Star, criado utilizando PHP e Design Patterns DAO. O usuário poderá realizar o cadastro no Movie Star, onde ele poderá adicionar e realizar reviews de filmes.
+
+## CRUD
+
+- Criar
+- Ler
+- Atualizar
+- Deletar
+
+## Diagrama Relacional
+
+![Diagrama Relacional](bd/bd.png)
+
+## Tecnologias
+
+![PHP](https://img.shields.io/badge/php-%2320232a.svg?style=for-the-badge&logo=php&logoColor=%)
+![CSS](https://img.shields.io/badge/css-%2320232a.svg?style=for-the-badge&logo=css3&logoColor=%2361dafb)
---
+++ 3-movieStar/index.php
@@ -0,0 +1,26 @@
+<?php 
+  require_once("globals.php");
+  require_once("database/db.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="short icon" href="img/moviestar.ico">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <!-- MovieStar CSS -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <!-- BOOTSTRAP JS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous"></script>
+</body>
+</html>
