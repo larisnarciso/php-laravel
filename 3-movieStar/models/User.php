340881f95ac9fc9a9fc03b2a61bc11ba6dcbafc6
Larissa Narciso <larisnarciso@gmail.com>
2023-05-11 17:05:22 -0300

    formulario de edicao de usuario e senha

--- 3-movieStar/models/User.php
+++ 3-movieStar/models/User.php
@@ -11,6 +11,10 @@
     public $bio;
     public $token;
 
+    public function getFullName(User $user){
+      return $user->name . " " . $user->lastname;
+    }
+
     public function generateToken(){
       return bin2hex(random_bytes(50));
     }
