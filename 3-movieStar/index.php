95ff9b2a655e9c2b299d3233d69cc56aec24acb4
Larissa Narciso <larisnarciso@gmail.com>
2023-05-12 02:19:54 -0300

    html e css da home

--- 3-movieStar/index.php
+++ 3-movieStar/index.php
@@ -3,7 +3,38 @@
 ?>
   
   <div id="main-container" class="container-fluid">
-    <h1>Corpo do Site</h1>
+
+    <h2 class="section-title">Filmes novos</h2>
+    <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
+    <div class="movies-container">
+      <div class="card movie-card">
+        <div class="card-img-top" style="background-image: url(<?= $BASE_URL ?>img/movies/movie_cover.jpg);"></div>
+        <div class="card-body">
+          <p class="card-rating">
+            <i class="fas fa-star"></i>
+              <span class="rating">9</span>
+          </p>
+          <h5 class="card-title">
+            <a href="#">Título do Filme</a>
+          </h5>
+          <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
+          <a href="#" class="btn btn-primary card-btn">Conhecer</a>
+        </div>
+      </div>
+    </div>
+
+    <h2 class="section-title">Ação</h2>
+    <p class="section-description">Veja os melhores filmes de ação</p>
+    <div class="movies-container">
+
+    </div>
+
+    <h2 class="section-title">Comédia</h2>
+    <p class="section-description">Veja os melhores filmes de comédia</p>
+    <div class="movies-container">
+
+    </div>
+
   </div>
 
 <?php
