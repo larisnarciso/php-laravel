d28b55b43f22bf878763bc15f8076b8a84c2239e
Larissa Narciso <larisnarciso@gmail.com>
2023-05-18 16:01:57 -0300

    view de filmes

--- 3-movieStar/dao/MovieDAO.php
+++ 3-movieStar/dao/MovieDAO.php
@@ -92,6 +92,20 @@
     }
 
     public function findById($id){
+      $movie = [];
+      $stmt = $this->conn->prepare("SELECT * FROM movies 
+                                    WHERE id = :id");
+      $stmt->bindParam(":id", $id);
+      
+      $stmt->execute();
+
+      if($stmt->rowCount() > 0){
+        $movieData = $stmt->fetch();
+        $movie = $this->buildMovie($movieData);
+        return $movie;
+      }else{
+        return false;
+      }
       
     }
 
