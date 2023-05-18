8915f5a552a6631878de20c9efa9a65936d3bee9
Larissa Narciso <larisnarciso@gmail.com>
2023-05-18 17:49:41 -0300

    edicao dos filmes

--- 3-movieStar/dao/MovieDAO.php
+++ 3-movieStar/dao/MovieDAO.php
@@ -136,7 +136,28 @@
     }
 
     public function update(Movie $movie){
-      
+      $stmt = $this->conn->prepare("UPDATE movies SET 
+      title = :title, 
+      description = :description, 
+      image = :image, 
+      trailer = :trailer, 
+      category = :category, 
+      length = :length
+      WHERE id = :id
+      ");
+
+      $stmt->bindParam(":title", $movie->title);
+      $stmt->bindParam(":description", $movie->description);
+      $stmt->bindParam(":image", $movie->image);
+      $stmt->bindParam(":trailer", $movie->trailer);
+      $stmt->bindParam(":category", $movie->category);
+      $stmt->bindParam(":length", $movie->length);
+      $stmt->bindParam(":id", $movie->id);
+
+      $stmt->execute();
+
+      // Mensagem de sucesso por editar filme
+      $this->message->setMessage("Filme editado com sucesso!", "sucess", "dashboard.php");
     }
 
     public function destroy($id){
--- 3-movieStar/movie_process.php
+++ 3-movieStar/movie_process.php
@@ -53,7 +53,7 @@
           } else {
             $imageFile = imagecreatefrompng($image["tmp_name"]);
           }
-
+          // Gerando o nome da imagem
           $imageName = $movie->generateImageName();
 
           imagejpeg($imageFile, "./img/movies/".$imageName, 100);
@@ -66,7 +66,7 @@
       }
       $movieDao->create($movie);
     }else $message->setMessage("Você precisa adicionar pelo menos: título, descrição e categoria!", "error", "back");
-  } else if($type === "delete"){
+  }else if($type === "delete"){
     // Recebe os dados do form
     $id = filter_input(INPUT_POST, "id");
 
@@ -82,7 +82,72 @@
     }else{
       $message->setMessage("Informações inválidas!", "error", "index.php");
     }
-  } else{
+
+  }else if($type === "update"){
+    // Recebe dados do input
+    $title = filter_input(INPUT_POST, "title");
+    $description = filter_input(INPUT_POST, "description");
+    $trailer = filter_input(INPUT_POST, "trailer");
+    $category = filter_input(INPUT_POST, "category");
+    $length = filter_input(INPUT_POST, "length");
+    $id = filter_input(INPUT_POST, "id");
+  
+    $movieData = $movieDao->findById($id);
+
+    // Verifica se encontrou o filme
+    if($movieData){
+      // Verificar se o filme é do usuário
+      if($movieData->users_id === $userData->id){
+
+        //  Validação minima de dados
+        if(!empty($title) && !empty($description) &&  !empty($category)){
+          
+          // Edição do filme
+          $movieData->title = $title;
+          $movieData->description = $description;
+          $movieData->trailer = $trailer;
+          $movieData->category = $category;
+          $movieData->length = $length;
+
+          // Upload de imagem do filme
+          if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+            
+            $image = $_FILES["image"];
+
+            // Checando tipo da imagem
+            if(in_array($image["type"], ["image/jpeg", "image/jpg", "image/png"])) {
+              
+              // Checa se é jpg
+              if(in_array($image["type"], ["image/jpeg", "image/jpg"])) {
+                $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+              } else {
+                $imageFile = imagecreatefrompng($image["tmp_name"]);
+              }
+              // Gerando o nome da imagem
+              $movie = new Movie();
+              $imageName = $movie->generateImageName();
+
+              imagejpeg($imageFile, "./img/movies/".$imageName, 100);
+
+              $movieData->image = $imageName;
+
+            } else {
+                $message->setMessage("Tipo inválido de imagem, envie jpg ou png!", "error", "editprofile.php");
+            }
+          }
+
+          $movieDao->update($movieData);
+        }else{
+          $message->setMessage("Você precisa adicionar pelo menos: título, descrição e categoria!", "error", "back");
+        }
+      }else{
+        $message->setMessage("Informações inválidas!", "error", "index.php");
+      }
+    }else{
+      $message->setMessage("Informações inválidas!", "error", "index.php");
+    }
+
+  }else{
     $message->setMessage("Informações inválidas!", "error", "index.php");
   }
 
