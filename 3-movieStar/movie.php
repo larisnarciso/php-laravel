428c44d07868aef2ab3493a13dfea5b701ea4e1c
Larissa Narciso <larisnarciso@gmail.com>
2023-05-19 02:29:21 -0300

    att

--- 3-movieStar/movie.php
+++ 3-movieStar/movie.php
@@ -4,7 +4,6 @@
   // Verifica se o usuário está autenticado
   require_once("models/Movie.php");
   require_once("dao/MovieDAO.php");
-  require_once("dao/ReviewDAO.php");
 
   // Pegar o id do filme
   $id = filter_input(INPUT_GET, "id");
@@ -12,7 +11,6 @@
   $movie;
 
   $movieDao = new MovieDAO($conn, $BASE_URL);
-  $reviewDao = new ReviewDAO($conn, $BASE_URL);
 
   if (empty($id)){
     $message->setMessage("O filme não foi encontrado!", "error", "index.php");
@@ -39,78 +37,73 @@
   }
 
   // Resgatar as reviews do filme
-  $movieReviews = $reviewDao->getMoviesReview($id);
+  // $movieReviews = $reviewDao->getMoviesReview($id);
 
   // Resgatar as reviews do filme
   $alreadyReviewed = false;
 
 ?>
 
-<div class="container-fluid" id="main-container">
-  <div class="row">
-    <div class="offset-md-1 col-md-6 movie-container">
-      <h1 class="page-title"><?= $movie->title ?></h1>
+  <div class="container-fluid" id="main-container">
+    <div class="row">
+      <div class="offset-md-1 col-md-6 movie-container">
+        <h1 class="page-title"><?= $movie->title ?></h1>
 
-      <p class="movie-datails">
-        <span>Duração: <?= $movie->length ?></span>
-        <span class="pipe"></span>
-        <span>Duração: <?= $movie->category ?></span>
-        <span class="pipe"></span>
-        <span><i class="fas fa-star"></i> 9</span>
-      </p>
+        <p class="movie-datails">
+          <span>Duração: <?= $movie->length ?></span>
+          <span class="pipe"></span>
+          <span>Duração: <?= $movie->category ?></span>
+          <span class="pipe"></span>
+          <span><i class="fas fa-star"></i> 9</span>
+        </p>
 
-      <div class="col-md-12 col d-flex justify-content-center ">
-      <div class="row align-items-center">
-        <iframe class="" src="<?= $movie->trailer ?>" width="560" height="315" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="col-md-12 col d-flex justify-content-center ">
+        <div class="row align-items-center">
+          <iframe class="" src="<?= $movie->trailer ?>" width="560" height="315" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        </div>
+        <div class="row justify-content-md-center p-description">
+          <p class="col col-lg-8"><?= $movie->description ?></p>
+        </div>
       </div>
+      <div class="row align-items-end">
+        <div class="offset-1 col-md-3 movie-image-container" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
       </div>
-      <div class="row justify-content-md-center p-description">
-        <p class="col col-lg-8"><?= $movie->description ?></p>
+        
+      <div class="offset-md-1 col-md-10" id="reviews-container">
+        <h3 id="reviews-title">Avaliações:</h3>
+        <!-- Verifica se habilita a review para o usuário ou não -->
+        <?php if(!empty($userData) && !$userOwnsMovie && !$alreadyReviewed): ?>
+        <div class="col-md-12" id="review-form-container">
+          <h4>Envie sua avaliação:</h4>
+          <p class="page-description">Preencha o formulário com a nota e comentário sobre o filme</p>
+          <form action="<?= $BASE_URL ?>review_process.php" id="review-form" method="POST">
+            <input type="hidden" name="type" value="create">
+            <input type="hidden" name="movies_id" value="<?= $movie->id ?>">
+            <div class="form-group">
+              <label for="rating">Nota do filme:</label>
+              <select name="rating" id="rating" class="form-control">
+                <option value="">Selecione</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="review">Seu comentário:</label>
+              <textarea name="review" id="review" rows="3" class="form-control" placeholder="O que você achou do filme?"></textarea>
+            </div>
+            <input type="submit" class="btn card-btn" value="Enviar comentário">
+          </form>
+        </div>
+        <?php endif ?>
+        <!-- Comentários -->
+        
       </div>
-    </div>
-    <div class="row align-items-end">
-      <div class="offset-1 col-md-3 movie-image-container" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
-    </div>
-      
-    <div class="offset-md-1 col-md-10" id="reviews-container">
-      <h3 id="reviews-title">Avaliações:</h3>
-      <!-- Verifica se habilita a review para o usuário ou não -->
-      <?php if(!empty($userData) && !$userOwnsMovie && !$alreadyReviewed): ?>
-      <div class="col-md-12" id="review-form-container">
-        <h4>Envie sua avaliação:</h4>
-        <p class="page-description">Preencha o formulário com a nota e comentário sobre o filme</p>
-        <form action="<?= $BASE_URL ?>review_process.php" id="review-form" method="POST">
-          <input type="hidden" name="type" value="create">
-          <input type="hidden" name="movies_id" value="<?= $movie->id ?>">
-          <div class="form-group">
-            <label for="rating">Nota do filme:</label>
-            <select name="rating" id="rating" class="form-control">
-              <option value="">Selecione</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="review">Seu comentário:</label>
-            <textarea name="review" id="review" rows="3" class="form-control" placeholder="O que você achou do filme?"></textarea>
-          </div>
-          <input type="submit" class="btn card-btn" value="Enviar comentário">
-        </form>
-      </div>
-      <?php endif ?>
-      <!-- Comentários -->
-      <?php foreach($movieReviews as $review): ?>
-        <?php require("templates/user_review.php"); ?>
-      <?php endforeach; ?>
-      <?php if(count($movieReviews) == 0): ?>
-        <p class="empty-list">Não há comentários para este filme ainda...</p>
-      <?php endif; ?>
     </div>
   </div>
-</div>
 
 
 <?php
