f1df1dda2406ce28fe500e8f4fcf7161c474d1ef
Larissa Narciso <larisnarciso@gmail.com>
2023-05-26 23:48:38 -0300

    correcao

--- 4-crud-laravel/resources/views/home.blade.php
+++ 4-crud-laravel/resources/views/home.blade.php
@@ -1,28 +1,27 @@
 @extends('layout.site')
 
-@section('titulo', 'Cursos')
+@section('titulo','Cursos')
 
 @section('conteudo')
-	<div class="container">
-    <h3 class="center">Lista de Cursos</h3>
-		@foreach($cursos as $curso)
-    	<div class="row">
-				<div class="col s12 m4">
-					<div class="card">
-						<div class="card-image">
-							<img src="{{asset($curso->imagem)}}">
-						</div>
-						<div class="card-content">
-							<h4>{{$curso->titulo}}</h4>
-							<p>{{$curso->descricao}}</p>
-						</div>
-						<div class="card-action">
-							<a href="#">Ver mais...</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		@endforeach
+  <div class="container">
+    <h3 class="center">Lista de cursos</h3>
+    <div class="row">
+      @foreach($cursos as $curso)
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="{{asset($curso->imagem)}}">
+            </div>
+            <div class="card-content">
+              <h4>{{$curso->titulo}}</h4>
+              <p>{{$curso->descricao}}</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Ver mais...</a>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
   </div>
-  
 @endsection
